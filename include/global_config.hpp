43350ab429744e4845ff64c26a861a8a8e3a6b6e
Moryu <moryu999@gmail.com>
2022-05-02 23:27:50 +0900

    feat : max stack size of tasks

--- include/global_config.hpp
+++ include/global_config.hpp
@@ -3,10 +3,31 @@
 
 #include <Arduino.h>
 
+/************************ RTOS設定 ここから ************************/
+#define ADT_STACk_SIZE      (1024)
+#define VDT_STACk_SIZE      (1024)
+#define FDT_STACk_SIZE      (1024)
+#define RMT_STACk_SIZE      (512)
+#define IDLETASK_STACk_SIZE (512)
+
+#define ADT_PRIORITY      (2)
+#define VDT_PRIORITY      (2)
+#define FDT_PRIORITY      (2)
+#define RMT_PRIORITY      (2)
+#define IDLETASK_PRIORITY (0)
+
+#define ENABLE_FREERTOS_TASK_STACK_PRINT (0) // IdleタスクでRTOSスタックサイズ測定を行うかどうか
+/************************ RTOS設定 ここまで ************************/
+
+/************************ DEBUG PRINT設定 ここから ************************/
+//#define DEBUG_PRINT_ADT(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
+#define DEBUG_PRINT_ADT(fmt, ...)
+
 //#define DEBUG_PRINT_FDT(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
-#define DEBUG_PRINT_FDT(fmt, ...) 
+#define DEBUG_PRINT_FDT(fmt, ...)
 
 //#define DEBUG_PRINT_VDT_IMU(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
-#define DEBUG_PRINT_VDT_IMU(fmt, ...) 
+#define DEBUG_PRINT_VDT_IMU(fmt, ...)
+/************************ DEBUG PRINT設定 ここまで ************************/
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,43 @@
 
-#include <FreeRTOS_TEENSY4.h>
 #include <Arduino.h>
+#include <FreeRTOS_TEENSY4.h>
 
-#include <micro_ros_arduino.h>
-#include <stdio.h>
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <std_msgs/msg/int32.h>
 #include <geometry_msgs/msg/twist.h>
+#include <micro_ros_arduino.h>
+#include <rcl/error_handling.h>
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/int32.h>
+#include <stdio.h>
 #include <test_msg/msg/robot_info.h>
 
 #include <QuadEncoder.h>
 
+#include "ArmDrive/AD_task_main.hpp"
 #include "FloorDetect/FD_task_main.hpp"
-#include "VehicleDrive/VD_task_main.hpp"
-#include "Utility/util_led.hpp"
 #include "Utility/util_cache.hpp"
+#include "Utility/util_led.hpp"
+#include "VehicleDrive/VD_task_main.hpp"
+#include "global_config.hpp"
 
 // RTOS heap
-uint8_t ucHeap[ configTOTAL_HEAP_SIZE ];
+uint8_t ucHeap[configTOTAL_HEAP_SIZE];
+
+// RTOS handle
+TaskHandle_t ArmDriveTask_handle     = NULL;
+TaskHandle_t VehicleDriveTask_handle = NULL;
+TaskHandle_t FloorDetectTask_handle  = NULL;
+TaskHandle_t IdleTask_handle         = NULL;
 
 QuadEncoder myEnc(2, 2, 3, 0);
 
-void setup_encoder(){
+void setup_encoder() {
   myEnc.setInitConfig();
   myEnc.init();
 }
+
+void idle_task(void *params);
 
 void setup() {
   UTIL::SCB_DisableDCache();
@@ -35,26 +45,45 @@ void setup() {
   Serial.begin(115200);
   setup_encoder();
   UTIL::init_LEDpin();
+  ADT::prepare_task();
   FDT::prepare_task();
   VDT::prepare_task();
 
   portBASE_TYPE s1;
-  s1 = xTaskCreate(FDT::main, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+  s1 = xTaskCreate(FDT::main, "FloorDetect", FDT_STACk_SIZE, NULL, FDT_PRIORITY, &FloorDetectTask_handle);
 
   // check for creation errors
-  if (s1 != pdPASS) {
+  if(s1 != pdPASS) {
     Serial.println("Creation problem");
-    while(1);
+    while(1)
+      ;
   }
-  
-  s1 = xTaskCreate(VDT::main, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
-  
+  s1 = xTaskCreate(VDT::main, "VehicleDrive", VDT_STACk_SIZE, NULL, VDT_PRIORITY, &VehicleDriveTask_handle);
+  s1 = xTaskCreate(ADT::main, "ArmDrive", ADT_STACk_SIZE, NULL, ADT_PRIORITY, &ArmDriveTask_handle);
+  s1 = xTaskCreate(idle_task, "Idle", IDLETASK_STACk_SIZE, NULL, IDLETASK_PRIORITY, &IdleTask_handle);
+
   vTaskStartScheduler();
 }
 
 void loop() {
   delay(1000);
   Serial.printf("Current position value1: %ld\r\n", myEnc.read());
+}
 
+void idle_task(void *params) {
+  while(1) {
+    vTaskDelay((1000L * configTICK_RATE_HZ) / 1000L);
+#if ENABLE_FREERTOS_TASK_STACK_PRINT
+    int ADT_max_stack_size      = ADT_STACk_SIZE - uxTaskGetStackHighWaterMark(ArmDriveTask_handle);
+    int VDT_max_stack_size      = VDT_STACk_SIZE - uxTaskGetStackHighWaterMark(VehicleDriveTask_handle);
+    int FDT_max_stack_size      = FDT_STACk_SIZE - uxTaskGetStackHighWaterMark(FloorDetectTask_handle);
+    int IdleTask_max_stack_size = IDLETASK_STACk_SIZE - uxTaskGetStackHighWaterMark(IdleTask_handle);
+
+    Serial.printf("[StackSize]ADT:%d,VDT:%d,FDT:%d,Idl:%d\n", ADT_max_stack_size,
+                  VDT_max_stack_size,
+                  FDT_max_stack_size,
+                  IdleTask_max_stack_size);
+#endif
+  }
 }
