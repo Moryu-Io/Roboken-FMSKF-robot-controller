9b2dd7ce43a0e00512b938bd0e042c033c699ba6
Moryu <moryu999@gmail.com>
2022-05-15 15:39:30 +0900

    style : Micro-ros setup function

--- src/RobotManager/RM_task_main.cpp
+++ src/RobotManager/RM_task_main.cpp
@@ -16,6 +16,9 @@
 #include <stdio.h>
 
 #include "../Utility/util_led.hpp"
+#include "../VehicleDrive/VD_task_main.hpp"
+
+bool is_microros_init_successful = false;
 
 // publisher
 rcl_publisher_t              pb_vchlInfo;
@@ -73,6 +76,14 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
 
 void sb_mecanumCmd_callback(const void *msgin) {
   const interfaces__msg__MecanumCommand *msg = (const interfaces__msg__MecanumCommand *)msgin;
+
+  VDT::MSG_REQ vdt_msg;
+  vdt_msg.common.MsgId         = VDT::MSG_ID::REQ_MOVE_DIR;
+  vdt_msg.move_dir.u32_cmd     = msg->cmd;
+  vdt_msg.move_dir.u32_time_ms = msg->time;
+  vdt_msg.move_dir.u32_speed   = msg->speed;
+
+  VDT::send_req_msg(&vdt_msg);
 }
 
 void sb_timeAngle_callback(const void *msgin) {
@@ -87,8 +98,6 @@ void srv_procSts_callback(const void *reqin, void *resout) {
 }
 
 namespace RMT {
-
-const byte DEV_MAC_ADDR[] = {0x04, 0xE9, 0xE5, 0x11, 0x97, 0x8B};
 IPAddress  device_ip(192, 168, 10, 177);
 IPAddress  agent_ip(192, 168, 10, 117);
 
@@ -98,13 +107,7 @@ static void get_dev_mac_addr(byte *_mac) {
   // Serial.printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", _mac[0], _mac[1], _mac[2], _mac[3], _mac[4], _mac[5]);
 }
 
-void prepare_task() {
-  byte mac_addr[8] = {};
-  get_dev_mac_addr(mac_addr);
-  set_microros_native_ethernet_udp_transports((byte *)mac_addr, device_ip, agent_ip, 9999);
-
-  delay(1000);
-
+static void create_microros_entities() {
   allocator = rcl_get_default_allocator();
 
   // create init_options
@@ -157,29 +160,48 @@ void prepare_task() {
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
-  //RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // RCCHECK(rclc_executor_add_timer(&executor, &timer));
   RCCHECK(rclc_executor_add_subscription(&executor, &sb_mcnmCmd, &msg_sb_mcnmCmd, &sb_mecanumCmd_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sb_tmAngle, &msg_sb_tmAngle, &sb_timeAngle_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_service(&executor, &srv_proc, &srv_req_procSts, &srv_res_procSts, srv_procSts_callback));
 
-//  while(1) {
-//    delay(100);
-//    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
-//  }
+  is_microros_init_successful = true;
+}
 
-  // RCCHECK(rcl_subscription_fini(&subscriber, &node));
-  // RCCHECK(rcl_node_fini(&node));
+void prepare_task() {
+  byte mac_addr[8] = {};
+  get_dev_mac_addr(mac_addr);
+  set_microros_native_ethernet_udp_transports((byte *)mac_addr, device_ip, agent_ip, 9999);
+
+  if(RMW_RET_OK == rmw_uros_ping_agent(50, 2)) {
+    create_microros_entities();
+  }
 }
 
 void main(void *params) {
+  uint32_t loop_tick = (int)configTICK_RATE_HZ / 120;
+
+
+
+  auto xLastWakeTime = xTaskGetTickCount();
   while(1) {
-    vTaskDelay((100L * configTICK_RATE_HZ) / 1000L);
-    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
-    
-    RCSOFTCHECK(rcl_publish(&pb_vchlInfo, &msg_pb_vhclInfo, NULL));
-    msg_pb_vhclInfo.pos.x++;
-    msg_pb_vhclInfo.pos.y--;
-    msg_pb_vhclInfo.pos.theta += 0.1f;
+    vTaskDelayUntil(&xLastWakeTime, loop_tick);
+
+    if(!is_microros_init_successful) {
+      if(RMW_RET_OK == rmw_uros_ping_agent(50, 2)) {
+        create_microros_entities();
+      } else {
+        // 何もしない
+      }
+    } else {
+      /* Micro-ROS接続完了時のRoutine */
+      rclc_executor_spin_some(&executor, RCUTILS_US_TO_NS(100));
+
+      RCSOFTCHECK(rcl_publish(&pb_vchlInfo, &msg_pb_vhclInfo, NULL));
+      msg_pb_vhclInfo.pos.x++;
+      msg_pb_vhclInfo.pos.y--;
+      msg_pb_vhclInfo.pos.theta += 0.1f;
+    }
   }
 }
 
