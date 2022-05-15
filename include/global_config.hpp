5a60d9cd78d0d509e787294de2dc2625e0627821
Moryu <moryu999@gmail.com>
2022-05-15 14:06:09 +0900

    feat : global config

--- include/global_config.hpp
+++ include/global_config.hpp
@@ -10,11 +10,14 @@
 #define RMT_STACk_SIZE      (1024)
 #define IDLETASK_STACk_SIZE (512)
 
-#define ADT_PRIORITY      (2)
+#define ADT_PRIORITY      (1)
 #define VDT_PRIORITY      (2)
-#define FDT_PRIORITY      (2)
+#define FDT_PRIORITY      (1)
 #define RMT_PRIORITY      (2)
 #define IDLETASK_PRIORITY (0)
+
+// MSG Bufferサイズ(Msg共用体サイズ何個分のバッファを用意するか)
+#define VDT_MSG_REQ_BUFFER_SIZE (3)
 
 #define ENABLE_FREERTOS_TASK_STACK_PRINT (0) // IdleタスクでRTOSスタックサイズ測定を行うかどうか
 /************************ RTOS設定 ここまで ************************/
@@ -25,6 +28,9 @@
 
 //#define DEBUG_PRINT_FDT(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
 #define DEBUG_PRINT_FDT(fmt, ...)
+
+//#define DEBUG_PRINT_VDT(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
+#define DEBUG_PRINT_VDT(fmt, ...)
 
 //#define DEBUG_PRINT_VDT_IMU(fmt, ...) Serial.printf(fmt, __VA_ARGS__)
 #define DEBUG_PRINT_VDT_IMU(fmt, ...)
