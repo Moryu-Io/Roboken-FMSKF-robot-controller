4920b4475e0583eb540012fc4ecea059f4d5483f
Moryu-Io <moryu.iori@gmail.com>
2024-11-17 00:26:44 +0900

    off debug print fdt

--- include/global_config.hpp
+++ include/global_config.hpp
@@ -57,8 +57,8 @@ void debug_printf(const char *format, Args const &...args) {
 //#define DEBUG_PRINT_STR_ADT(fmt) debug_printf(fmt)
 #define DEBUG_PRINT_STR_ADT(fmt)
 
-#define DEBUG_PRINT_FDT(fmt, ...) debug_printf(fmt, __VA_ARGS__)
-//#define DEBUG_PRINT_FDT(fmt, ...)
+//#define DEBUG_PRINT_FDT(fmt, ...) debug_printf(fmt, __VA_ARGS__)
+#define DEBUG_PRINT_FDT(fmt, ...)
 
 //#define DEBUG_PRINT_VDT(fmt, ...) debug_printf(fmt, __VA_ARGS__)
 #define DEBUG_PRINT_VDT(fmt, ...)
